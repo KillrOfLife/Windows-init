133f7110faea6ee7feb62a75f2529dbb6305ca0a
Maxim Claeys <mxmclaeys@gmail.com>
2023-04-17 21:08:49 +0200

    [Deej] Fixed the bootscript

--- Deej/bootscript.ps1
+++ Deej/bootscript.ps1
@@ -1,2 +1,2 @@
 #update config.yml from repo
-Invoke-RestMethod https://gitlab.com/maxim.claeys/windows-init/-/raw/main/deej/config.yml -o "$env:APPDATA\_custom\deej\config.yml"
+Invoke-RestMethod https://gitlab.com/maxim.claeys/windows-init/-/raw/main/Deej/config.yaml -o "$env:APPDATA\_custom\deej\config.yaml"
